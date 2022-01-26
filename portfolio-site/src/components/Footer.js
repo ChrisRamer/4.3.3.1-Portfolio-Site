c106cff169edc7e4524a7e9f30af601c42e01246
Chris Ramer <chrisramer.97@gmail.com>
2022-01-26 09:54:08 -0800

    Craete project

---
+++ portfolio-site/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div id="footer">Made with <span role="img" aria-label="Heart">❤️</span> by <b>Chris Ramer (Chris is Awesome)</b> | Proudly hosted on <a href="https://github.com/ChrisRamer/4.3.3.1-Portfolio-Site">GitHub</a></div>
+	)
+}
+
+export default Footer;
